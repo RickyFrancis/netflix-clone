3f8411e9bc5077abcc17704a3565462688721d9b
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-06-23 23:41:03 +0600

    First Commit

---
+++ src/components/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import '../styles/Banner.css';
+
+function Banner() {
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundPosition: "center center",
+        color: '#fff'
+      }}
+    >
+     <div class="banner__contents">
+       <h1 class="banner__title">Movie Name</h1>
+       <div class="banner__buttons">
+         <button class="banner__button">Play</button>
+         <button class="banner__button">My List</button>
+       </div>
+       <h1 class="banner__description">This is a test description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam culpa rem vel aliquid! Architecto vitae fugiat, facilis fugit cupiditate officia error optio deserunt nam nesciunt magni sit aspernatur minus voluptatem, eligendi excepturi! Rerum, odit vero dolorem dignissimos voluptatibus explicabo maiores vel sequi commodi illum ipsa quod maxime quas delectus eaque autem? Quidem nesciunt atque at beatae temporibus architecto ex commodi exercitationem voluptates nihil earum magnam eaque, debitis pariatur fugit amet. Enim repellat fuga laudantium, natus harum consequuntur ut eius veniam soluta laborum, ab voluptate officia. Eos nesciunt quasi ab quam illum vel! Nam itaque officiis dolorum quisquam accusantium repudiandae modi pariatur sit unde labore quidem quis hic quae obcaecati incidunt ad laborum maiores sed minus, dolor, perspiciatis eius nisi quasi? Iusto dicta excepturi repellendus atque necessitatibus molestias omnis quo nemo fuga natus deleniti, nobis quibusdam unde fugit, eveniet porro minima nostrum vel magnam quae! Iste vitae dolor quo ab aliquid quia perferendis suscipit. Atque delectus error, asperiores minus nostrum nesciunt impedit dolorum, fugit quaerat molestias non repellendus. Soluta sunt beatae laboriosam illo hic voluptatem eum sit. Labore, quasi. Magni exercitationem cumque nesciunt id eveniet ut quis corporis officiis dignissimos nobis vel officia tempore consequuntur praesentium, non laudantium. Cupiditate laborum consectetur, suscipit saepe adipisci labore culpa iste obcaecati ipsa sapiente. Asperiores non ea a earum ullam, eaque adipisci nisi et libero nam sunt incidunt praesentium, repellat rem quae illo dolores iure assumenda nesciunt. Ducimus rerum minus odio consequatur illo sapiente odit. Dolore consequatur quidem debitis omnis facilis ducimus molestiae maxime facere quasi natus quam, totam maiores repellendus accusamus iste sit doloremque animi soluta saepe velit? Ut vitae dolores iusto vero alias necessitatibus unde recusandae tenetur corporis totam illum impedit, earum officiis sed assumenda accusamus repellat doloremque sunt et ducimus aut quos, autem sit est? Repellendus autem provident reiciendis quam enim voluptate earum pariatur consequatur et optio, vitae possimus, quia repellat mollitia odit? Ipsa recusandae nulla quia repudiandae, ea consectetur maxime quo sunt! Ullam dolore maiores a vel! Repellendus quasi quis dolore facere explicabo amet nemo perspiciatis ratione! Eos, quod ullam? Sed voluptas, non tenetur, quos dolor quam sint recusandae nisi sunt minus libero ab magnam inventore dolore? Officia, nulla non totam dolores, quibusdam numquam praesentium iste in maiores odio quos illo dolorum nostrum fugit repudiandae earum rem. Voluptatibus, nulla! Delectus explicabo facere soluta inventore, sunt consectetur cumque eos vel deserunt provident earum fuga exercitationem magnam ad beatae porro, praesentium sit iste libero ipsam iusto placeat cum? Cum, et labore eum amet culpa aliquam. Deserunt inventore placeat corrupti dignissimos ipsam magnam laborum quibusdam quidem eius, a qui. Iste itaque tempora repellat similique consequuntur voluptatibus magnam distinctio alias velit facilis, enim pariatur dolorem molestiae assumenda accusamus. Iusto veniam repudiandae sunt quo expedita, at omnis, facere vitae quibusdam numquam minus deserunt molestias, id perferendis. Tenetur laudantium cumque vitae accusantium nobis animi quod quaerat suscipit est? Quaerat sint doloribus aliquid quas veritatis, fugit quae corrupti aperiam autem amet provident omnis sequi maiores, eius consequuntur ducimus quibusdam id. Aliquam modi saepe doloremque veritatis autem. Eius, voluptates?</h1>
+     </div>
+
+     <div class="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB8XzH_PPvpINWfwu-yLSIWmGH4O_-gOeo',
+  authDomain: 'netflix-clone-953f6.firebaseapp.com',
+  projectId: 'netflix-clone-953f6',
+  storageBucket: 'netflix-clone-953f6.appspot.com',
+  messagingSenderId: '1002723713939',
+  appId: '1:1002723713939:web:5654b85dcaa8e09d6c7e66',
+  measurementId: 'G-C2ZC5YPNHZ',
+};
