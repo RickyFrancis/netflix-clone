10f53b4c7d176367837bee7c7dbc890cc432d542
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-06-25 23:52:04 +0600

    Backdrop on banner from tmdb api using axios

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,32 +1,56 @@
-import React from "react";
-import '../styles/Banner.css';
+import React, { useEffect, useState } from "react";
+import "../styles/Banner.css";
+import requests from "../Requests";
+import axios from "axios";
 
 function Banner() {
+  const [movie, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      setMovies(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+
+      return request;
+    };
+
+    fetchData();
+  }, []);
 
   const truncate = (string, n) => {
-    return string?.length > n ? string.substr(0, n-1) + '...' : string;
-  }
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
 
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
         backgroundPosition: "center center",
-        color: '#fff'
+        color: "#fff",
       }}
     >
-     <div class="banner__contents">
-       <h1 class="banner__title">Movie Name</h1>
-       <div class="banner__buttons">
-         <button class="banner__button">Play</button>
-         <button class="banner__button">My List</button>
-       </div>
-       <h1 class="banner__description">{truncate(`This is a test description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam culpa rem vel aliquid! Architecto vitae fugiat, facilis fugit cupiditate officia error optio deserunt nam nesciunt magni sit aspernatur minus voluptatem, eligendi excepturi! Rerum, odit vero dolorem dignissimos voluptatibus explicabo maiores vel sequi commodi illum ipsa quod maxime quas delectus eaque autem? Quidem nesciunt atque at beatae temporibus architecto ex commodi exercitationem voluptates nihil earum magnam eaque, debitis pariatur fugit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut aspernatur doloribus, tempora perferendis aliquam dolor in pariatur explicabo, dicta veritatis esse incidunt, reprehenderit totam quisquam? Expedita, ipsum. Repudiandae animi officiis, ullam quae consequatur architecto atque corporis quas eaque quos explicabo amet fugiat esse vel ipsum. Saepe hic omnis qui soluta excepturi quae quisquam debitis maiores placeat aspernatur vitae minima, molestiae, eligendi, molestias repudiandae. Sequi rem, ipsum, ad suscipit earum nihil tempore dolores in sit fuga voluptas. Suscipit temporibus nesciunt quibusdam dignissimos? Labore, aliquam! Debitis, nesciunt. Tempora amet saepe eveniet aperiam repellat, cupiditate quos voluptas reprehenderit error nobis facilis modi?`, 150)}</h1>
-     </div>
+      <div class="banner__contents">
+        <h1 class="banner__title">Movie Name</h1>
+        <div class="banner__buttons">
+          <button class="banner__button">Play</button>
+          <button class="banner__button">My List</button>
+        </div>
+        <h1 class="banner__description">
+          {truncate(
+            `This is a test description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam culpa rem vel aliquid! Architecto vitae fugiat, facilis fugit cupiditate officia error optio deserunt nam nesciunt magni sit aspernatur minus voluptatem, eligendi excepturi! Rerum, odit vero dolorem dignissimos voluptatibus explicabo maiores vel sequi commodi illum ipsa quod maxime quas delectus eaque autem? Quidem nesciunt atque at beatae temporibus architecto ex commodi exercitationem voluptates nihil earum magnam eaque, debitis pariatur fugit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut aspernatur doloribus, tempora perferendis aliquam dolor in pariatur explicabo, dicta veritatis esse incidunt, reprehenderit totam quisquam? Expedita, ipsum. Repudiandae animi officiis, ullam quae consequatur architecto atque corporis quas eaque quos explicabo amet fugiat esse vel ipsum. Saepe hic omnis qui soluta excepturi quae quisquam debitis maiores placeat aspernatur vitae minima, molestiae, eligendi, molestias repudiandae. Sequi rem, ipsum, ad suscipit earum nihil tempore dolores in sit fuga voluptas. Suscipit temporibus nesciunt quibusdam dignissimos? Labore, aliquam! Debitis, nesciunt. Tempora amet saepe eveniet aperiam repellat, cupiditate quos voluptas reprehenderit error nobis facilis modi?`,
+            150
+          )}
+        </h1>
+      </div>
 
-     <div class="banner--fadeBottom" />
+      <div class="banner--fadeBottom" />
     </header>
   );
 }
