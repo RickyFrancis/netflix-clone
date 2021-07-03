45982dda6366a9c9925c532014a747a0f10ddb97
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-07-03 23:21:41 +0600

    Introduced react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import HomeScreen from "./components/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route> */}
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
